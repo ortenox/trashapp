42dd7c4b0bdcdc60e9ac7d17cc60aa366291f343
ortenox <108170217+ortenox@users.noreply.github.com>
2023-06-11 17:07:02 +0700

    Delete CAPSTONE-PROJECT-C23-PC614c-main/CAPSTONE-PROJECT-C23-PC614c-main directory

--- CAPSTONE-PROJECT-C23-PC614c-main/CAPSTONE-PROJECT-C23-PC614c-main/cc-smarttrash-app-main/bin/config/config.js
+++
@@ -1,8 +0,0 @@
-let config = {
-  host: "34.101.202.120",
-  user: "cc",
-  password: "askirul7",
-  database: "smarttrash_db",
-};
-
-module.exports = config;
--- CAPSTONE-PROJECT-C23-PC614c-main/CAPSTONE-PROJECT-C23-PC614c-main/cc-smarttrash-app-main/bin/dbconnection.js
+++
@@ -1,5 +0,0 @@
-let mysql = require("mysql2");
-let config = require("./config/config.js");
-let db = mysql.createConnection(config);
-
-module.exports = db;
